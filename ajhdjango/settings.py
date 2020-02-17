90124ce392a36b88948dab3720a4cdc4b7711b81
forFuture <code.lover1110@gmx.com>
2020-02-17 05:45:25 +0100

    change setting

--- ajhdjango/settings.py
+++ ajhdjango/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'ajhdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # DATABASES = {
@@ -91,19 +91,19 @@ DATABASES = {
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'sql_server.pyodbc',
-#         'NAME': 'ShopeVote',
-#         'USER': 'ShopVoteAdmin',
-#         'PASSWORD':'Shop2020!',
-#         'HOST':'shopvote.database.windows.net',
-#         'PORT': '',
-#         'OPTIONS': {
-#             'driver': 'SQL Server Native Client 11.0',
-#         }
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'ShopeVote',
+        'USER': 'ShopVoteAdmin',
+        'PASSWORD':'Shop2020!',
+        'HOST':'shopvote.database.windows.net',
+        'PORT': '',
+        'OPTIONS': {
+            'driver': 'SQL Server Native Client 11.0',
+        }
+    }
+}
 
 
 
